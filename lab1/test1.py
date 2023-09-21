e4d2bc172d0563aa976fe26a6449a159332488e4
wangzupeng12061 <wangzupeng12061@gmail.com>
2023-09-21 11:42:07 +0800

    lab1

---
+++ lab1/test1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 21 09:26:46 2023
+
+@author: wangzupeng
+"""
+
+import numpy as np
+import cv2
+img = cv2.imread('test.png',0)  #试着修改 0 为 1.
+cv2.imshow('image',img)
+k = cv2.waitKey(0)
+if k == 27:         # wait for ESC key to exit
+    cv2.destroyAllWindows()
+elif k == ord('s'): # wait for 's' key to save and exit
+    cv2.imwrite('test2.jpg',img)
+    cv2.destroyAllWindows()
